d4dd7f5178e84312fed765feecda2109e0df66ee
Maria Jose <mriveram@ucenfotec.ac.cr>
2020-03-27 22:19:20 -0600

    WIP Tarea 3

---
+++ Tarea#3/Tarea#3/Controllers/AddProductViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddProductViewController.swift
+//  Tarea#3
+//
+//  Created by Maria Jose on 3/27/20.
+//  Copyright © 2020 Maria Jose. All rights reserved.
+//
+
+import UIKit
+
+class AddProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+  
+
+}
---
+++ Tarea#3/Tarea#3/Controllers/ListProductsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tarea#3
+//
+//  Created by Maria Jose on 3/26/20.
+//  Copyright © 2020 Maria Jose. All rights reserved.
+//
+
+import UIKit
+
+class ListProductsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
