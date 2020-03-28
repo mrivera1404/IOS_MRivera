19e14de1101d8449ebba5f1e688c84e06305e0c2
Maria Jose <mriveram@ucenfotec.ac.cr>
2020-03-28 08:40:53 -0600

    Quiz 3

---
+++ Quiz#2/Quiz#2/Controllers/MultiplierViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MultiplierViewController.swift
+//  Quiz#2
+//
+//  Created by Maria Jose on 3/28/20.
+//  Copyright © 2020 Maria Jose. All rights reserved.
+//
+
+import UIKit
+
+class MultiplierViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
+
+extension MultiplierViewController: UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    
+}
---
+++ Quiz#2/Quiz#2/CustomCells/MultTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MultTableViewCell.swift
+//  Quiz#2
+//
+//  Created by Maria Jose on 3/28/20.
+//  Copyright © 2020 Maria Jose. All rights reserved.
+//
+
+import UIKit
+
+class MultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
